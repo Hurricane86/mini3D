c0311bc3b33c52c99f7108d62bfa75862ba94d77
Jeff <jeff@mini3d.org>
2010-11-19 16:27:22 +0100

    Removed IGraphicsService from mini3d.h

--- mini3d.h
+++ mini3d.h
@@ -3,13 +3,13 @@
 // This file is part of mini3d <www.mini3d.org>
 // It is distributed under the MIT Software License <www.mini3d.org/license>
 
+#ifdef _WIN32
 #define NOMINMAX
+#endif
 
 #include "mini3d/ogl20/OGL20GraphicsService.h"
 
 #ifdef _WIN32
 #include "mini3d/d3d9/D3D9GraphicsService.h"
 #endif
-
-#include "mini3d/IGraphicsService.h"
 
